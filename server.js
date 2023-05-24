00e3a4241b6fcc50f07636241bf9e379e595fc30
Maelikah <eva.longoria@gmail.com>
2023-05-24 13:14:01 -0600

    Created the starer logic for the server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+// Declare variables  
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Require api route
+const apiRouter = require("./routes/api");
