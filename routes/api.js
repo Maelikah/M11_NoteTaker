b894988a2fe676899810417e0573509fe8b4f603
Maelikah <eva.longoria@gmail.com>
2023-05-24 16:22:29 -0600

    Added more functionality to the api.js

--- routes/api.js
+++ routes/api.js
@@ -8,10 +8,14 @@ const fs = require("fs");
 let notes = require("../db/db.json");
 // Use the uniqid package 
 const uniqid = require("uniqid");
+1// Use the color coding custom middleware
+const { clog } = require('./middleware/clog');
+
+// Import custom middleware, "clog to handle color coded console logs
+router.use(clog);
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON
 router.get("/notes", (req, res) => {
-    console.info(`${req.method} request received for tips`);
     res.json(notes);
 });
 
@@ -20,7 +24,39 @@ router.get("/notes", (req, res) => {
 router.post("/notes", (req, res) => {
     const {title, text} = req.body;
 
-    if
+    // add unique id to note
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uniqid(),
+        };
+    
+        // Push new note into the json file
+        notes.push(newNote);
+
+        // Stringify notes
+        let noteString = JSON.stringify(notes);
+
+        // Overwrite file with new data
+        fs.writeFile(`../db/db.json`, noteString, (err) =>
+        err
+            ? console.error(err)
+            : console.log (`Sucessfully added a new note`)
+        );
+
+        console.log(response);
+        // 201 = request has succeeded and has led to the creation of a resource
+        res.status(201).json(response);
+    } 
+    
+    else {
+        // 500 = server-side error
+        res.status(500).json('Error in adding note');
+    }
+    
+
+    
 
 }); 
 
