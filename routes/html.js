c11b9988e97d17653c7d9bfe49c492fbbb1d9ee6
Maelikah <eva.longoria@gmail.com>
2023-05-24 14:57:25 -0600

    Completed the html.js file

--- routes/html.js
+++ routes/html.js
@@ -1,1 +1,19 @@
-const express
+// Declare dependencies
+const express = require("express");
+const path = require("path");
+// Use the router function included in express  
+const router = express.Router();
+
+// Get index.html as main page
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../routes/public/index.html"))
+});
+
+// Get notes.html 
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../routes/public/notes.html"))
+});
+
+module.exports = router;
